cc6bbca58ca1782b746421afc8c5c52a6c02f7a1
Greg Todd <reflectivecode@users.noreply.github.com>
2016-05-03 15:25:06 -0500

    add backup script

---
+++ backup.py
@@ -0,0 +1,21 @@
+import json, sys, os, subprocess, time
+
+src      = os.environ['BACKUP_SRC']
+dest     = os.environ['BACKUP_DEST']
+interval = float(os.environ['BACKUP_INTERVAL'])
+
+while True:
+  cmd = ["aws", "s3", "sync", src, dest, "--delete", "--exclude", "cache/*/*"]
+
+  with open(os.path.join(src, "cache/.sinopia-db.json")) as data_file:
+    data = json.load(data_file)
+
+  for package in data["list"]:
+    cmd.append("--include")
+    cmd.append("cache/" + package + "/*")
+
+  code = subprocess.call(cmd)
+  if code:
+    sys.exit(code)
+
+  time.sleep(interval)
