e29d63f9f304f281aab46b1856dda94895cf6bf5
Bruce <bruce.harrison@asynchrony.com>
2016-10-18 15:56:53 -0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -7,13 +7,15 @@ interval = float(os.environ['BACKUP_INTERVAL'])
 while True:
   cmd = ["aws", "s3", "sync", src, dest, "--delete", "--exclude", "cache/*/*"]
   
-  if os.path.isfile(os.path.join(src, "cache/.sinopia-db.json")):
-    with open(os.path.join(src, "cache/.sinopia-db.json")) as data_file:
-      data = json.load(data_file)
-    for package in data["list"]:
-      cmd.append("--include")
-      cmd.append("cache/" + package + "/*")
-
+  try:
+        with open(os.path.join(src, "cache/.sinopia-db.json")) as data_file:
+          data = json.load(data_file)
+        for package in data["list"]:
+          cmd.append("--include")
+          cmd.append("cache/" + package + "/*")
+  except IOError as e:
+        print os.path.join(src, "cache/.sinopia-db.json") + " could not be found"
+      
   code = subprocess.call(cmd)
   if code:
     sys.exit(code)
